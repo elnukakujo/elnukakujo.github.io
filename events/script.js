8fb50d6cb55a8f7f11cb3c26777e764bb3a01b56
elnukakujo <jagernoe1s3@hotmail.com>
2024-06-19 22:56:50 -0400

    Correction of menu and side bar bug

--- events/script.js
+++ events/script.js
@@ -1,17 +1,19 @@
 window.addEventListener('scroll', function() {
     var nav = document.getElementById('nav');
     var menu = document.getElementById('menu-toggle');
+    var sidebar = document.getElementById('sidebar');
     if (window.scrollY > 50) { // Change 50 to the scroll threshold you want
         nav.classList.add('scrolled');
         menu.classList.add('scrolled');
     } else {
         nav.classList.remove('scrolled');
         menu.classList.remove('scrolled');
+        sidebar.classList.remove('visible');
+        menu.classList.remove('menu-open');
     }
 });
-
 document.querySelector('#menu-toggle').addEventListener('click', function() {
-    this.classList.toggle('menu-open');
+    this.classList.add('menu-open');
     var sidebar = document.getElementById('sidebar');
-    sidebar.classList.toggle('visible');
-  });
+    sidebar.classList.add('visible');
+});
