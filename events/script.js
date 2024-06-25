53eda5026e595be7d8fa4b9466b1923083656898
elnukakujo <jagernoe1s3@hotmail.com>
2024-06-25 11:55:59 -0400

    Updated sidebar

--- events/script.js
+++ events/script.js
@@ -1,19 +1,11 @@
 window.addEventListener('scroll', function() {
     var nav = document.getElementById('nav');
-    var menu = document.getElementById('menu-toggle');
     var sidebar = document.getElementById('sidebar');
-    if (window.scrollY > 50) { // Change 50 to the scroll threshold you want
+    if (window.scrollY > 80) { // Change 50 to the scroll threshold you want
         nav.classList.add('scrolled');
-        menu.classList.add('scrolled');
+        sidebar.classList.add('scrolled');
     } else {
         nav.classList.remove('scrolled');
-        menu.classList.remove('scrolled');
-        sidebar.classList.remove('visible');
-        menu.classList.remove('menu-open');
+        sidebar.classList.remove('scrolled');
     }
-});
-document.querySelector('#menu-toggle').addEventListener('click', function() {
-    this.classList.toggle('menu-open');
-    var sidebar = document.getElementById('sidebar');
-    sidebar.classList.toggle('visible');
 });
