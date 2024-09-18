36180423600f23d59df86a8b80bed30438d82c30
elnukakujo <jagernoe1s3@hotmail.com>
2024-09-18 19:49:15 -0400

    removed the bar dropdowns

--- mywebsite/src/components/Sidebar.js
+++ mywebsite/src/components/Sidebar.js
@@ -26,21 +26,8 @@ export default function Sidebar(){
             <div className="sidebar_content">
                 <Link to="/">Home</Link>
                 <Link to="/aboutme">About Me</Link>
-                <div className="dropdown">
-                    <Link to="/education" className="drop-trigger">Education</Link>
-                    <div className="dropdown-content">
-                        <Link to="/education#masterUdem">Master</Link>
-                        <Link to="/education#bacUniLu">Bachelor</Link>
-                        <Link to="/education#oc">Online Courses</Link>
-                    </div>
-                </div>
-                <div className="dropdown">
-                    <Link to="/projects" className="drop-trigger">Projects</Link>
-                    <div className="dropdown-content">
-                        <Link to="/projects#master_projects">Master Projects</Link>
-                        <Link to="/projects#bachelor_projects">Bachelor Projects</Link>
-                    </div>
-                </div>
+                <Link to="/education">Education</Link>
+                <Link to="/projects">Projects</Link>
                 <a 
                 href="https://raw.githubusercontent.com/elnukakujo/elnukakujo.github.io/main/mywebsite/src/assets/docs/cv.pdf" 
                 target="_blank" 
