b7b096924089819c611f82141acea11a48ca1aaa
elnukakujo <jagernoe1s3@hotmail.com>
2024-09-30 22:20:32 -0400

    onlineCourses bug fixed

--- mywebsite/src/pages/Education.js
+++ mywebsite/src/pages/Education.js
@@ -1,11 +1,6 @@
 import React from 'react';
 import courseraLogo from '../assets/img/logo/coursera_logo.jpg';
-import udemLogo from '../assets/img/logo/udem_logo.jpg';
-import uniluLogo from '../assets/img/logo/unilu_logo.jpg';
-import { useEffect } from "react";
-
-import udemEte from '../assets/img/index/udem_ete.jpg';
-import udemNeige from '../assets/img/index/udem_neige.jpg';
+import { useEffect, useState } from "react";
 
 import '../assets/css/pages/education.css';
 
@@ -35,15 +30,14 @@ function Images({images}){
   )
 }
 
-function Description({anchorId, title, date, GPA, courses, onlineCourses, websiteLink, images}){
-  if (onlineCourses) {
-    return (
-      <div>
+function OnlineCourses({courses}){
+  return (
+    <div>
         <div className="anchor" id="oc"></div>
         <div className="description" id="oc-content">
           <h4>Online Courses</h4>
           <ul>
-            {onlineCourses.map((course, index) => (
+            {courses.map((course, index) => (
               <li key={index}>
                 <a href={course[1]} target="_blank" rel="noreferrer">
                   {course[0]}
@@ -56,8 +50,10 @@ function Description({anchorId, title, date, GPA, courses, onlineCourses, websit
           </ul>
         </div>
       </div>
-    );
-  }
+  );
+}
+
+function University({anchorId, title, date, GPA, courses, websiteLink, images}){
   const output = [];
 
   if (date) {
@@ -110,77 +106,50 @@ function Description({anchorId, title, date, GPA, courses, onlineCourses, websit
 }
 
 export default function Education(){
+
+    const [universities, setUniversities] = useState([]);
+    const [onlineCourses, setOnlineCourses] = useState([]);
+
+    const fetchEducation = async () => {
+        try {
+            const response = await fetch('https://raw.githubusercontent.com/elnukakujo/elnukakujo.github.io/refs/heads/main/mywebsite/src/assets/data/education.json');
+            if (!response.ok) {
+                console.log(response);
+                throw new Error('Network response was not ok');
+            }
+            const data = await response.json();
+            setUniversities(data.uni); 
+            setOnlineCourses(data.ol);
+        } catch (error) {
+            console.error('There has been a problem with your fetch operation:', error);
+        }
+    };
     useEffect(() => {
       document.title = 'Education | Noe Jager';
+      fetchEducation();
     }, []);
+    
+    console.log(onlineCourses);
+
     return (
         <SectionOpener id="education" title="Education">
-            <Description
-              anchorId={"masterUdem"}
-              title={"Master in Computer Science at Université de Montréal"}
-              date={"September 2022 - Upcoming"}
-              GPA={"2.9"}
-              courses={[
-                "Data Science", "Machine Learning Fundamentals", "Data Visualization", "Model based software engineering",
-                "Empirical methods in HCI", "Tridimensional vision"
-              ]}
-              websiteLink={[
-                "https://admission.umontreal.ca/programmes/maitrise-en-informatique/",
-                udemLogo
-              ]}
-              images={[
-                [
-                  udemEte,
-                  "https://admission.umontreal.ca/programmes/maitrise-en-informatique/",
-                  "The campus of Université de Montréal in the summer"
-                ],
-                [
-                  udemNeige,
-                  "https://admission.umontreal.ca/programmes/maitrise-en-informatique/",
-                  "The campus of Université de Montréal in the winter"
-                ]
-              ]}
-            />
-            <Description
-              anchorId={"bacUniLu"}
-              title={"Bachelor of Science in Computer Science at University of Luxembourg"}
-              websiteLink={[
-                "https://www.uni.lu/fstm-en/study-programs/bachelor-in-computer-science/",
-                uniluLogo
-              ]}
-              date={"September 2019 - July 2022"}
-              GPA={"3.0"}
-              courses={["Web development", "Software Engineering", "Networking and Communication", "Cybersecurity 1 and 2", 
-              "Human Computer Interaction and User Centered Design"]}
-              images={[
-                [
-                  "https://www.uni.lu/wp-content/uploads/sites/9/2023/07/Belval_Maison-Savoir_est_Brumat-Photo-Foersom-sarl_2013-1.jpg",
-                  "https://www.uni.lu/en/about/campuses/belval-campus/",
-                  "View from the sky of the campus of Belval where my classes took place"
-                ],
-                [
-                  "https://www.ft.com/__origami/service/image/v2/images/raw/https%3A%2F%2Fwww.ft.com%2F__origami%2Fservice%2Fimage%2Fv2%2Fimages%2Fraw%2Fhttp%253A%252F%252Fcom.ft.imagepublish.upp-prod-eu.s3.amazonaws.com%252Fc27a1fea-3af2-11e9-b856-5404d3811663%3Fsource%3Dnext-article%26fit%3Dscale-down%26quality%3Dhighest%26width%3D700%26dpr%3D1?source=next-opengraph&fit=scale-down&width=900",
-                  "https://www.ft.com/content/249fd948-35cc-11e9-9be1-7dc6e2dfa65e",
-                  "The campus was built on a former industrial site, which is now a modern and vibrant area"
-                ]
-              ]}
-            />
-            <Description
-              onlineCourses={[
-                  [
-                    "Design in Art and Concepts Specialization (Coursera - CalArts)",
-                    "https://www.coursera.org/specializations/game-design"
-                  ],
-                  [
-                    "Data Mining Specialization by the University of Illinois at Urbana-Champaign (Coursera)",
-                    "https://www.coursera.org/specializations/data-mining"
-                  ],
-                  [
-                    "Deep Learning Specialization by Deeplearning.ai (Coursera)",
-                    "https://www.coursera.org/specializations/deep-learning"
-                  ]
-              ]}
-            />
+            {universities.map((university, index) => (
+              <University
+                key={index}
+                anchorId={university.anchorId}
+                title={university.title}
+                date={university.date}
+                GPA={university.GPA}
+                courses={university.courses}
+                websiteLink={university.websiteLink}
+                images={university.images}
+              />
+            ))}
+            {
+              <OnlineCourses
+                courses={onlineCourses}
+              />
+            }
         </SectionOpener>
     )
 }
